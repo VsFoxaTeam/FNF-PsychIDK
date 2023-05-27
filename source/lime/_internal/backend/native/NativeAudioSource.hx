7169af1307cf1e9d50401cf17b4537565410e247
Kooker The Fox! <62410083+KookerFoxYT@users.noreply.github.com>
2023-05-27 16:47:20 -0400

    25 mins to 3 hours
    Co-authored-by: Alex <85291330+TheLeerName@users.noreply.github.com>

--- source/lime/_internal/backend/native/NativeAudioSource.hx
+++ source/lime/_internal/backend/native/NativeAudioSource.hx
@@ -136,7 +136,7 @@ class NativeAudioSource
 			}
 		}
 
-		samples = Std.int((dataLength * 8) / (parent.buffer.channels * parent.buffer.bitsPerSample));
+		samples = Std.int((dataLength) / ((parent.buffer.channels * parent.buffer.bitsPerSample) / 8));
 	}
 
 	public function play():Void
@@ -487,7 +487,9 @@ class NativeAudioSource
 			return length;
 		}
 
-		return Std.int(samples / parent.buffer.sampleRate * 1000) - parent.offset;
+		var thelength:Int = Std.int(samples / parent.buffer.sampleRate * 1000) - parent.offset;
+		if (thelength < 0) thelength = 12173936;
+		return thelength;
 	}
 
 	public function setLength(value:Int):Int
